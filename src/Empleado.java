ed0dfbd8ba2b66af712d15951e522867eba1afa7
maripachon <pachonmaritza102@gmail.com>
2025-04-08 10:23:22 -0500

    Update Empleado.java

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public abstract class Empleado {
+    protected String nombre;
+    protected int edad;
 
-public class Empleado {
+    public Empleado(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Método abstracto
+    public abstract double calcularSalario();
 }
+
