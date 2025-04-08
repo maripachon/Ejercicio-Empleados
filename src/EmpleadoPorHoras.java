60568b74a13131559640a4b44c2c17ad57fd71f3
maripachon <pachonmaritza102@gmail.com>
2025-04-08 10:24:08 -0500

    Update EmpleadoPorHoras.java

--- src/EmpleadoPorHoras.java
+++ src/EmpleadoPorHoras.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public class EmpleadoPorHoras extends Empleado {
+    private double pagoPorHora;
+    private int horasTrabajadas;
 
-public class EmpleadoPorHoras {
+    public EmpleadoPorHoras(String nombre, int edad, double pagoPorHora, int horasTrabajadas) {
+        super(nombre, edad);
+        this.pagoPorHora = pagoPorHora;
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public double getPagoPorHora() {
+        return pagoPorHora;
+    }
+
+    public void setPagoPorHora(double pagoPorHora) {
+        this.pagoPorHora = pagoPorHora;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return pagoPorHora * horasTrabajadas;
+    }
 }
