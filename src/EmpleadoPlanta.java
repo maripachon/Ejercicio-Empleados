5b2353dce237b4bffb03650f5b354e6edf70e658
maripachon <pachonmaritza102@gmail.com>
2025-04-08 10:23:46 -0500

    Update EmpleadoPlanta.java

--- src/EmpleadoPlanta.java
+++ src/EmpleadoPlanta.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public class EmpleadoPlanta extends Empleado {
+    private static double SALARIO_FIJO = 2100000;
 
-public class EmpleadoPlanta {
+    public EmpleadoPlanta(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return SALARIO_FIJO;
+    }
 }
+
